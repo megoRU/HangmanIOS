13f0f8bb29959606cbdf62f5f9d7e9f94b88bb0d
mego <savin.yura2014@gmail.com>
2025-07-30 15:20:42 +0300

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by mego on 30.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
